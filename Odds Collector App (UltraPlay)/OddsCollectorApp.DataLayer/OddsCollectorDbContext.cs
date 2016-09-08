25256665a3c04c5f382071345bd16c4dad9bb4c6
Mihail Mihaylov <mishomihaylov91@gmail.com>
2016-09-08 14:21:36 +0300

    Update OddsCollectorDbContext.cs

--- Odds Collector App (UltraPlay)/OddsCollectorApp.DataLayer/OddsCollectorDbContext.cs
+++ Odds Collector App (UltraPlay)/OddsCollectorApp.DataLayer/OddsCollectorDbContext.cs
@@ -8,19 +8,10 @@ namespace OddsCollectorApp.DataLayer
 
     public class OddsCollectorDbContext : DbContext
     {
-        // Your context has been configured to use a 'OddsCollectorDbContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'OddsCollectorApp.DataLayer.OddsCollectorDbContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'OddsCollectorDbContext' 
-        // connection string in the application configuration file.
         public OddsCollectorDbContext()
             : base("name=OddsCollectorDbContext")
         {
         }
-
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
         public virtual DbSet<Sport> Sports { get; set; }
         public virtual DbSet<Event> Events { get; set; }
